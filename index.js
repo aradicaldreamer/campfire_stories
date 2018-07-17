de9b2eabd6a6669a6437318fa697f727cb1192ba
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2018-07-17 17:27:56 +0100

    Added functions to generate grammar and add characters with user input

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var tracery = require('tracery-grammar'); // import Tracery for tacery generativ
 // var should = require('chai').should(); // adding chai for the node tracery examples
 var Improv = require('improv'); // import Improv for Improv generative text examples;
 var P5 = require('p5'); // Adding p5.js libraries for Processing;
-var rita = require('rita');
+require('rita');
 require('p5/lib/addons/p5.sound');
 require('p5/lib/addons/p5.dom');
 
@@ -51,20 +51,43 @@ require('p5/lib/addons/p5.dom');
 // ];
 
 // Rita Code
-var example = "This is a test";
-var rs = rita.RiString(example);
+// var example = "This is a test";
+// var rs = RiString(example);
+
+// debugger;
+
+// Data Functions
+
+function loadData () {
+  try {
+    return JSON.parse(window.localStorage.getItem("story"));
+  }
+  catch (error) {
+    console.log(error.message);
+    return;
+  }
+}
+
 
 // Tracery code
 
-var story = {
+var story = loadData() || {
   "start": "#[hero:#character#]story#",
   "character": ["dragon", "unicorn", "rainbow"],
-  "story": "A #adj# #hero# fights the #adj# monster. Go #hero# go! Hope you liked the story, PLAYER!",
+  "story": "A #adj# #hero# fights the #adj# monster. Go #hero# go!",
   "adj": ["dark", "sleepy", "quiet"]
 }
 
-var grammar = tracery.createGrammar(story);
+var grammar;
 
+
+function generateStory () {
+
+  grammar = tracery.createGrammar(story);
+  var traceryText = grammar.flatten("#start#");
+  var rs = RiString(traceryText);
+  return rs;
+}
 
 // Processing Code
 
@@ -72,17 +95,24 @@ var input;
 
 function processingCode(p) {
     
+  // story.character[0] = 'cat'
+  // story.character.push('cat')
     // console.log(p);
     // console.log(p.select);
     // console.log(p.getAudioContext);
     // console.log(lines.join('\n')); // testing to see if we have access to Improv Text in Processing
     // var improvText = lines.join('\n'); // taking Improv text and saving as a variable separated by new lines
-    var traceryText = grammar.flatten("#start#");
+    // var traceryText;
+    // var rs;
+    
+    // var traceryText = grammar.flatten("#start#");
+    // var rs = RiString(traceryText);
 
+    var rs = generateStory();
 
     p.setup = function () {
       p.createCanvas(640, 480);
-      p.userInput();
+      userInput();
     }
   
     p.draw = function () {
@@ -90,11 +120,11 @@ function processingCode(p) {
       p.fill(255);
       p.textSize(32);
       // p.text(improvText,100,100,400,400);
-      p.text(traceryText,100,100,400,400);
+      p.text(rs.text(),100,100,400,400);
     }
 
     // This function is intended to collect user input at various points during the story
-    p.userInput = function () {
+    function userInput () {
       input = p.createInput();
       input.position(50, p.height - 100);
       input.size(p.width - 100, 50);
@@ -102,7 +132,23 @@ function processingCode(p) {
       input.style("color", "white");
       input.style("border-color", "white");
       input.style("font-size", "4em");
+      input.changed(newCharacter); 
     }
+
+    function newCharacter () {
+      // Removing all special characters from input
+      var val = input.value();
+      val = val.replace(/[#\[\]]/g, '');
+
+      story.character.push(val);
+      window.localStorage.setItem("story", JSON.stringify(story));
+      input.value('');
+      rs = generateStory();
+      console.log(story.character);
+    }
+
+
+
   }
   
   new P5(processingCode);
