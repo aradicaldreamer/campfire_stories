091da798917f3093715b0a09df4f079044816aec
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2018-08-13 10:54:18 +0100

    Rewrote Original Prototype to Focus on Tracery with User Interaction

--- index.js
+++ index.js
@@ -1,94 +1,33 @@
 // Required Libraries
 var Jquery = require('jquery'); // adding jquery as needed by Tracery
 var tracery = require('tracery-grammar'); // import Tracery for tacery generative text tests;
-// var should = require('chai').should(); // adding chai for the node tracery examples
-var Improv = require('improv'); // import Improv for Improv generative text examples;
 var P5 = require('p5'); // Adding p5.js libraries for Processing;
 require('rita');
-require('p5/lib/addons/p5.sound');
-require('p5/lib/addons/p5.dom');
+require('p5/lib/addons/p5.sound.js');
+require('p5/lib/addons/p5.dom.js');
 
-// Improv Example from Documentation
-// var spec = {
-//   animal: {
-//     groups: [
-//       {
-//         tags: [['class', 'mammal']],
-//         phrases: ['dog', 'cat']
-//       },
-//       {
-//         tags: [['class', 'bird']],
-//         phrases: ['parrot']
-//       }
-//     ]
-//   },
-//   root: {
-//     groups: [
-//       {
-//         tags: [],
-//         phrases: [
-//           "[name]: I have a [:animal] who is [#2-7] years old."
-//         ]
-//       }
-//     ]
-//   }
-// };
-
-// var spec = require('./specs/example.js'); // this is an alternative for bringing this code into the main script
-
-// var improv = new Improv(spec, {
-//   filters: [Improv.filters.mismatchFilter()]
-// });
-
-// var bob = { name: 'Bob' };
-// var alice = { name: 'Alice', tags: [['class', 'mammal']] };
-// var carol = { name: 'Carol', tags: [['class', 'bird']] };
-
-// var lines = [
-//   improv.gen('root', bob),
-//   improv.gen('root', alice),
-//   improv.gen('root', carol)
-// ];
-
-// Rita Code
-// var example = "This is a test";
-// var rs = RiString(example);
-
-// debugger;
 
 // Data Functions
 
-function loadData () {
-  try {
-    return JSON.parse(window.localStorage.getItem("story"));
-  }
-  catch (error) {
-    console.log(error.message);
-    return;
-  }
-}
-
-
 // Tracery code
 
-var story = loadData() || {
-  "start": "#[hero:#character#][villain:#monster#]story#",
-  "character": ["dragon", "unicorn", "rainbow"],
-  "monster": ["tiger"],
-  "story": "A #adj# #hero# fights the #adj# #villain#. Go #hero# go!",
-  "adj": ["dark", "sleepy", "quiet"]
-}
+// var initialVariables = {
+//   start: "#[hero:#names#]story#",
+//   names: ["Matthew", "Alex", "Ben", "Doruk", "Ece", "Hugh", "Jade", "Julia", "Phoenix", "Raymond", "Tommy"],
+//   story: "This is a story about #hero# and when they met someone new. Who did they meet?",
+//   sceneTwo: "#hero# met #friendName# on a bus. Where did they go?",
+//   sceneThree: "#hero# and #friendName# went to #location#. They had so much fun!"
+// }
 
-var grammar;
+var initialVariables = require('./grammars/story.js');
 
+// function generateStory () {
 
-function generateStory () {
-
-  grammar = tracery.createGrammar(story);
-  var traceryText = grammar.flatten("#start#");
-  var rs = RiString(traceryText);
-  return rs;
-}
+//   grammar = tracery.createGrammar(initialVariables);
+//   var traceryText = grammar.flatten("#start#");
+//   var rs = RiString(traceryText);
+//   return rs;
+// }
 
 // Processing Code
 
@@ -109,19 +48,33 @@ function processingCode(p) {
     // var traceryText = grammar.flatten("#start#");
     // var rs = RiString(traceryText);
 
-    var rs = generateStory();
+    grammar = tracery.createGrammar(initialVariables);
+    var hero = grammar.flatten("#names#");
+    grammar.pushRules("hero", [hero]);
+    var traceryText = grammar.flatten("#story#");
+    var rs = traceryText;
+
+    var scene = 1;
 
     p.setup = function () {
-      p.createCanvas(640, 480);
-      userInput();
+      p.createCanvas(p.windowWidth, p.windowHeight);
+        userInput();
+    }
+
+    p.update = function () {
+
     }
 
     p.draw = function () {
       p.background(0);
       p.fill(255);
       p.textSize(32);
-      // p.text(improvText,100,100,400,400);
-      p.text(rs.text(),100,100,400,400);
+      p.textAlign(p.CENTER);
+      p.text(rs,0,0,p.windowWidth-50,p.windowHeight - 100);
+    }
+
+    p.windowResized = function () {
+      resizeCanvas(p.windowWidth, p.windowHeight);
     }
 
     // This function is intended to collect user input at various points during the story
@@ -132,20 +85,29 @@ function processingCode(p) {
       input.style("background-color", "#000000");
       input.style("color", "white");
       input.style("border-color", "white");
-      input.style("font-size", "4em");
-      input.changed(newCharacter);
+      input.style("font-size", "2em");
+      input.changed(newStoryElement);
     }
 
-    function newCharacter () {
+    function newStoryElement () {
       // Removing all special characters from input
-      var val = input.value();
-      val = val.replace(/[#\[\]]/g, '');
+      //rs = "This is a story about when " + characterName.text() + " met " + grammar.flatten("#friendName#");
+      if (scene === 1) {
+        var val = input.value();
+        val = val.replace(/[#\[\]]/g, '');
+        grammar.pushRules("friendName", [val]);
+        input.value('');
+        rs = grammar.flatten("#sceneTwo#");
+        scene = 2;
+      } else if (scene === 2) {
+        var val = input.value();
+        val = val.replace(/[#\[\]]/g, '');
+        grammar.pushRules("location", [val]);
+        input.value('');
+        rs = grammar.flatten("#sceneThree#")
+        scene = 3;
+      }
 
-      story.monster.push(val);
-      window.localStorage.setItem("story", JSON.stringify(story));
-      input.value('');
-      rs = generateStory();
-      console.log(story.character);
     }
 
 
