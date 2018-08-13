a7f21f138bca316a66da825cd6ec4e6e5854cae2
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2018-08-13 10:53:37 +0100

    Created example grammar and moved it to it's own folder to be required later

---
+++ grammars/story.js
@@ -0,0 +1,7 @@
+module.exports = {
+    start: "#story#",
+    names: ["Matthew", "Alex", "Ben", "Doruk", "Ece", "Hugh", "Jade", "Julia", "Phoenix", "Raymond", "Tommy"],
+    story: "This is a story about #hero# and when they met someone new. Who did they meet?",
+    sceneTwo: "#hero# met #friendName# on a bus. Where did they go?",
+    sceneThree: "#hero# and #friendName# went to #location#. They had so much fun!"
+};
